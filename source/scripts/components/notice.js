89eeebf6bd7f599d7030e13f025b8ea911b8fbfe
Josh Magness <magnessjo@me.com>
2017-08-25 21:21:01 -0400

    Update notice for responsiveness

--- source/scripts/components/notice.js
+++ source/scripts/components/notice.js
@@ -2,16 +2,32 @@
 // Variables
 
 const container = document.querySelector('.notice');
+const text = container.querySelector('.text');
 const button = container.querySelector('h1');
 const wrapper = container.querySelector('.expand-content');
+const padding = parseInt(window.getComputedStyle(text).getPropertyValue('padding-top'));
+
+// Resize
+
+function resize() {
+
+  const height = wrapper.querySelector('div').offsetHeight + padding * 2;
+  wrapper.style.height = `${height}px`;
+  wrapper.style.transition = 'none';
+
+}
 
 // Export
 
 export default function() {
 
+  console.log(padding);
+
   button.addEventListener('click', () => {
 
-    const height = wrapper.querySelector('div').offsetHeight;
+    const height = wrapper.querySelector('div').offsetHeight + padding * 2;
+
+    wrapper.style.transition = 'height 1s';
 
     if  (wrapper.offsetHeight == 0) {
       wrapper.style.height = `${height}px`;
@@ -22,5 +38,7 @@ export default function() {
     }
 
   });
+
+  window.addEventListener('resize', resize);
 
 }
