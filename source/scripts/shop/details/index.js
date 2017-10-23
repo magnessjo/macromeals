97e113ddd15b93f77140b473013a71e2ca1b3fa9
Josh Magness <magnessjo@me.com>
2017-10-23 11:03:48 -0400

    Cleanup form on shop details page

--- source/scripts/shop/details/index.js
+++ source/scripts/shop/details/index.js
@@ -4,12 +4,11 @@
 import productCalculation from 'scripts/helpers/form/productCalculation.js';
 import pieChart from 'scripts/components/pieChart.js';
 import fixedElement from 'scripts/helpers/fixedElement.js';
-import fixedForm from './fixedForm.js';
 import factTab from './fact-tab.js';
 
 // Variables
 
-const container = document.querySelector('.product-actions');
+const container = document.querySelector('.flex-actions');
 const element = container.querySelector('.fixed-wrapper');
 
 // Load
