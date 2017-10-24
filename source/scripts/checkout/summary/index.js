30df6105096e78328ae52437fc3d448ae871c7df
Josh Magness <magnessjo@me.com>
2017-10-24 17:09:44 -0400

    Add summary remove functionality

--- source/scripts/checkout/summary/index.js
+++ source/scripts/checkout/summary/index.js
@@ -2,11 +2,13 @@
 // Imports
 
 import modifyFormValueForResize from 'scripts/helpers/form/modifyFormValueForResize.js';
+import removeLineItem from './removeLineItem.js';
 
 // Load
 
 document.addEventListener('DOMContentLoaded', () => {
 
+  removeLineItem();
   modifyFormValueForResize();
 
 });
---
+++ source/scripts/checkout/summary/removeLineItem.js
@@ -0,0 +1,82 @@
+
+// Imports
+
+import findParentNode from 'scripts/helpers/findParentNode.js';
+import removeCartItem from 'scripts/helpers/cart/removeCartItem.js';
+import displayCartTotalInHeader from 'scripts/helpers/cart/displayCartTotalInHeader.js';
+
+// Variables
+
+const body = document.querySelector('body');
+const form = document.querySelector('form');
+const buttons = Array.from(form.querySelectorAll('.remove'));
+
+// Show modal
+
+function showModal(button) {
+
+  const parent = findParentNode(button, 'order-item');
+  const title = parent.querySelector('h2');
+  const size = parent.querySelector('h2 span');
+
+  const modal = document.createElement('div');
+  const text = document.createElement('p');
+
+  const buttonWrapper = document.createElement('div');
+  const cancelButton = document.createElement('button');
+  const confirmButton = document.createElement('button');
+
+  function removeModal() {
+    modal.remove();
+  }
+
+  modal.classList.add('modal');
+
+  buttonWrapper.classList.add('button-wrapper');
+  cancelButton.classList.add('cancel');
+  confirmButton.classList.add('confirm');
+
+  text.innerHTML = `
+    Please confirm that you would like to remove ${title.innerHTML} from your cart
+  `;
+
+  cancelButton.innerHTML = 'Cancel';
+  cancelButton.addEventListener('click', removeModal);
+
+  confirmButton.innerHTML = 'Confrim';
+  confirmButton.addEventListener('click', () => {
+
+    const id = parent.getAttribute('data-id');
+    let data = `${window.csrfTokenName}=${window.csrfTokenValue}&lineItemId=${id}`;
+
+    removeCartItem(data).then(() => {
+
+      parent.remove();
+      removeCartItem();
+      removeModal();
+      displayCartTotalInHeader();
+
+    });
+
+  });
+
+  buttonWrapper.appendChild(cancelButton);
+  buttonWrapper.appendChild(confirmButton);
+  modal.appendChild(buttonWrapper);
+  modal.appendChild(text);
+  body.appendChild(modal);
+
+}
+
+// Export
+
+export default function() {
+
+  buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      e.preventDefault();
+      showModal(button);
+    });
+  })
+
+}
