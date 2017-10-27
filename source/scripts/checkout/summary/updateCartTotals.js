8853a13d9985d07dbc1ef3e6ae311de4c3c680e0
Josh Magness <magnessjo@me.com>
2017-10-27 12:08:27 -0400

    Update summary page for dynamic shipping cost

--- source/scripts/checkout/summary/updateCartTotals.js
+++ source/scripts/checkout/summary/updateCartTotals.js
@@ -7,8 +7,6 @@ import formatStringForDollar from 'scripts/helpers/formatStringForDollar.js';
 
 export default function(cart) {
 
-  console.log(cart);
-
   const cartTotal = document.querySelector('.cart-total');
 
   if (cartTotal) {
@@ -16,14 +14,17 @@ export default function(cart) {
     const totalElement = cartTotal.querySelector('[data-type="total-amount"]');
     const subTotalElement = cartTotal.querySelector('[data-type="items-amount"]');
     const taxAmount = cartTotal.querySelector('[data-type="tax-amount"]');
+    const shippingAmount = cartTotal.querySelector('[data-type="shipping-amount"]');
 
     const totalNumber = formatStringForDollar(cart.totalPrice);
     const subTotalNumber = formatStringForDollar(cart.itemSubtotal);
     const taxNumber = formatStringForDollar(cart.totalTax);
+    const shippingNumber = formatStringForDollar(cart.baseShippingCost);
 
     totalElement.innerHTML = totalNumber;
     subTotalElement.innerHTML = subTotalNumber;
     taxAmount.innerHTML = taxNumber;
+    shippingAmount.innerHTML = shippingNumber;
 
   }
 
