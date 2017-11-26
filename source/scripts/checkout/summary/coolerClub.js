153b2e56c548f93094407154d722fbd6acaa6f81
Josh Magness <magnessjo@me.com>
2017-11-26 14:49:47 -0500

    Add cooler club opiton

--- source/scripts/checkout/summary/coolerClub.js
+++ source/scripts/checkout/summary/coolerClub.js
@@ -1,12 +1,14 @@
 
 // Import
 
+import fetchPostData from 'scripts/helpers/fetchPostData.js';
 import postToCart from 'scripts/helpers/cart/postToCart.js';
 import updateCartTotals from 'scripts/helpers/cart/updateCartTotals.js';
 
 // Variables
 
 const container = document.querySelector('.cooler-club');
+const cartTotal = document.querySelector('#cart-total');
 
 // Export
 
@@ -20,13 +22,25 @@ export default function() {
 
       e.preventDefault();
 
-      let postData = `${window.csrfTokenName}=${window.csrfTokenValue}`;
+      let data = `${window.csrfTokenName}=${window.csrfTokenValue}`;
 
-      postToCart(postData).then( (data) => {
+      fetchPostData(data, '/actions/MacroCommerce/Cart/setCoolerClub').then((response) => {
 
-        console.log(data);
+        if (response.success) {
 
-        const cart = data.cart;
+          const shippingElement = cartTotal.querySelector('[data-type="shipping-amount"]');
+          const totalElement = cartTotal.querySelector('[data-type="total-amount"]');
+          const totalCost = totalElement.innerHTML;
+          const costString = totalCost.replace(/[^0-9.]/g, '');
+          const cost = costString - 10.00;
+
+          shippingElement.innerHTML = '0.00';
+          totalElement.innerHTML = `$${cost}`;
+          button.disabled = true;
+
+        } else {
+          console.log(response);
+        }
 
       });
 
