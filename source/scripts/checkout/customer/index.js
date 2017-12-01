5bd3064ace88ebce2e75c9031b1a475d58751a9b
Josh Magness <magnessjo@me.com>
2017-12-01 08:02:47 -0500

    remove pickup option
    Crazy update for sales

--- source/scripts/checkout/customer/index.js
+++ source/scripts/checkout/customer/index.js
@@ -4,8 +4,8 @@
 import modifyFormValueForResize from 'scripts/helpers/form/modifyFormValueForResize.js';
 import inputQuery from 'scripts/helpers/form/inputQuery.js';
 import formSubmit from './formSubmit.js';
-import shippingMethod from './shippingMethod.js';
-import zipCode from './zipCode.js';
+// import shippingMethod from './shippingMethod.js';
+// import zipCode from './zipCode.js';
 import notice from 'scripts/components/notice.js';
 
 // Variables
@@ -19,8 +19,8 @@ document.addEventListener('DOMContentLoaded', () => {
   modifyFormValueForResize();
   inputQuery(form);
   formSubmit();
-  shippingMethod();
-  zipCode();
+  // shippingMethod();
+  // zipCode();
   notice();
 
 });
