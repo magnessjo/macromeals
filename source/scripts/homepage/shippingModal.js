337da0b282d65d548495068ef84f0b5dd5e07e49
Josh Magness <magnessjo@me.com>
2017-12-02 12:37:02 -0500

    Update shipping cost modal on homepage

--- source/scripts/homepage/shippingModal.js
+++ source/scripts/homepage/shippingModal.js
@@ -25,15 +25,22 @@ function setRate() {
 
   const url = '/actions/MacroCommerce/ShippingRates';
   const zip = shippingForm.querySelector('input[name="zipCode"]').value;
-  const quantity = shippingForm.querySelector('input[name="quantity"]').value;
-  let data = `${window.csrfTokenName}=${window.csrfTokenValue}&zip=${zip}&quantity=${quantity}`;
+  let data = `${window.csrfTokenName}=${window.csrfTokenValue}&zip=${zip}&quantity=1`;
 
   fetchPostData(data,url).then( (response) => {
 
     loaderAnimation.style.display = 'none';
 
     if (response.success) {
-      result.innerHTML = `<p>Your Estimate<span>$${response.amount.toFixed(2)}</span></p>`;
+      const cost = parseInt(response.amount);
+      const isGround = response.type == 'ground';
+
+      if (isGround) {
+        result.innerHTML = `<p>Your Estimate<span>$${cost.toFixed(2)}</span></p><p class="">Orders over $50 ship for $10.00</p>`;
+      } else {
+        result.innerHTML = `<p>Your Estimate<span>$${cost.toFixed(2)}</span></p>`;
+      }
+
       inputs.forEach( (input) => input.value = '');
     } else {
       result.innerHTML = `There was an error getting your estimate.  Please contact us directly at <a href="info@macromeals.life">info@macromeals.life</a> for an estimate.`;
