44848bfd3d44e7e4b4a13981fedd2c624a6f6680
Josh Magness <magnessjo@me.com>
2017-12-10 13:19:18 -0500

    Update homepage

--- source/scripts/homepage/index.js
+++ source/scripts/homepage/index.js
@@ -1,15 +1,25 @@
 
 // Import
 
+<<<<<<< Updated upstream
 // import filter from './filter.js';
 import shippingModal from './shippingModal.js';
+=======
+// import shippingModal from './shippingModal.js';
+import lazyLoadFoodByCategory from 'scripts/common/lazyLoadFoodByCategory.js';
+>>>>>>> Stashed changes
 
 // Load
 
 document.addEventListener('DOMContentLoaded', () => {
 
+<<<<<<< Updated upstream
   // filter();
 
   shippingModal();
+=======
+  lazyLoadFoodByCategory();
+  // shippingModal();
+>>>>>>> Stashed changes
 
 });
---
+++ source/scripts/shop/meals/index.js
@@ -0,0 +1,14 @@
+
+// Import
+
+// import filter from './filter.js';
+import lazyLoadFoodByCategory from 'scripts/common/lazyLoadFoodByCategory.js';
+
+// Load
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // filter();
+  lazyLoadFoodByCategory();
+
+});
