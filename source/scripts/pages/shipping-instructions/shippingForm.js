228f6a7c6804283bb86169d9d48e301dd3157b64
Josh Magness <magnessjo@me.com>
2017-12-15 16:44:01 -0500

    Update the hompage to remove shipping

---
+++ source/scripts/pages/shipping-instructions/shippingForm.js
@@ -0,0 +1,81 @@
+
+// import
+
+import fetchPostData from 'scripts/helpers/fetchPostData.js';
+import inputQuery from 'scripts/helpers/form/inputQuery.js';
+import checkFormFieldsForValidAttribute from 'scripts/helpers/form/checkFormFieldsForValidAttribute.js';
+import findParentNode from 'scripts/helpers/findParentNode.js';
+
+// Variables
+
+const shippingForm = document.querySelector('#shipping-calculator');
+const loaderAnimation = shippingForm.querySelector('.loader');
+const result = shippingForm.querySelector('.result');
+
+const inputs = shippingForm.querySelectorAll('input:not([type="submit"])')
+const submit = shippingForm.querySelector('input[type="submit"]');
+
+
+// Set Rate
+
+function setRate() {
+
+  const url = '/actions/MacroCommerce/ShippingRates';
+  const zip = shippingForm.querySelector('input[name="zipCode"]').value;
+  let data = `${window.csrfTokenName}=${window.csrfTokenValue}&zip=${zip}&quantity=1`;
+
+  fetchPostData(data,url).then( (response) => {
+
+    loaderAnimation.style.display = 'none';
+
+    if (response.success) {
+      const cost = parseInt(response.amount);
+      const isGround = response.type == 'ground';
+
+      if (isGround) {
+        if (response.itemSubtotal > 50) {
+          result.innerHTML = `<p>Your Estimate<span>$${cost.toFixed(2)}</span></p>`;
+        } else {
+          result.innerHTML = `
+            <p>Your Estimate<span>$${cost.toFixed(2)}</span></p>
+            <p class="">Orders over $50 ship for $10.00</p>
+          `;
+        }
+
+      } else {
+        result.innerHTML = `<p>Your Estimate<span>$${cost.toFixed(2)}</span></p>`;
+      }
+
+      inputs.forEach( (input) => input.value = '');
+    } else {
+      result.innerHTML = `There was an error getting your estimate.  Please contact us directly at <a href="info@macromeals.life">info@macromeals.life</a> for an estimate.`;
+    }
+
+  });
+
+}
+
+// Export
+
+export default function() {
+
+  shippingForm.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    loaderAnimation.style.display = 'block';
+    inputQuery(shippingForm, false);
+
+    const isValid = checkFormFieldsForValidAttribute(inputs);
+    if (isValid) setRate();
+
+    ga('send', {
+      hitType: 'event',
+      eventCategory: 'Shipping quote',
+      eventAction: 'Show',
+      eventLabel: 'Get Shipping Quote'
+    });
+
+  });
+
+}
