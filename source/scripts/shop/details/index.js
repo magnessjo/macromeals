0a127753b761f1db67eee2207e41e7f31a35e166
Josh Magness <magnessjo@me.com>
2017-12-26 18:06:45 -0500

    Update details page top styling

--- source/scripts/shop/details/index.js
+++ source/scripts/shop/details/index.js
@@ -1,23 +1,18 @@
 
 // Imports
 
-import productCalculation from 'scripts/helpers/form/productCalculation.js';
+// import productCalculation from 'scripts/helpers/form/productCalculation.js';
 import pieChart from 'scripts/components/pieChart.js';
-import fixedElement from 'scripts/helpers/fixedElement.js';
 import factTab from './fact-tab.js';
 
 // Variables
-
-const container = document.querySelector('.flex-actions');
-const element = container.querySelector('.fixed-wrapper');
 
 // Load
 
 window.addEventListener('load', () => {
 
-  productCalculation();
+  // productCalculation();
   pieChart();
-  fixedElement(container, element);
   factTab();
 
 });
