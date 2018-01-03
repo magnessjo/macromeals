a15cad71e689fa42e107ff79c7fdbe65a702adca
Josh Magness <magnessjo@me.com>
2018-01-03 13:05:26 -0500

    Update dummy payment

--- source/scripts/checkout/billing/dPayment.js
+++ source/scripts/checkout/billing/dPayment.js
@@ -6,53 +6,56 @@ import fetchPostData from 'scripts/helpers/fetchPostData.js';
 // Variables
 
 const form = document.querySelector('form#dummy');
-const inputs = Array.from(form.querySelectorAll('input[type="text"]'));
-const submitButton = form.querySelector('input[type="submit"]');
 
 // Export
 
 export default function() {
 
-  form.addEventListener('submit', (e) => {
+  if (form) {
 
-    e.preventDefault();
-    submitButton.disabled = true;
+    const inputs = Array.from(form.querySelectorAll('input[type="text"]'));
+    const submitButton = form.querySelector('input[type="submit"]');
 
-    let post = `${window.csrfTokenName}=${window.csrfTokenValue}&paymentMethodId=3&`;
+    form.addEventListener('submit', (e) => {
 
-    inputs.forEach( (input, i) => {
+      e.preventDefault();
+      submitButton.disabled = true;
 
-      const name = input.getAttribute('name');
-      const value = input.value;
+      let post = `${window.csrfTokenName}=${window.csrfTokenValue}&paymentMethodId=3&`;
 
-      post += `${name}=${value}`;
-      if (i != inputs.length - 1) post += '&';
+      inputs.forEach( (input, i) => {
+
+        const name = input.getAttribute('name');
+        const value = input.value;
+
+        post += `${name}=${value}`;
+        if (i != inputs.length - 1) post += '&';
+
+      });
+
+      fetchPostData(post, '/actions/commerce/payments/pay').then( (response) => {
+
+        if (response.success) {
+          window.location.href = '/checkout/complete';
+        }
+
+        if (response.error) {
+
+          submitButton.disabled = false;
+
+          errorElement.innerHTML = `
+            <p>${response.error}</p>
+            <p>Your cart has been saved.  Please contact our support team at <a href="help@macromeals.life">help@macromeals.life</a> to process your payment.</p>
+          `;
+
+          errorElement.style.display = 'block';
+        }
+
+      });
 
     });
 
-    console.log(post);
 
-    fetchPostData(post, '/actions/commerce/payments/pay').then( (response) => {
-      console.log(response);
-
-      if (response.success) {
-        window.location.href = '/checkout/complete';
-      }
-
-      if (response.error) {
-
-        submitButton.disabled = false;
-
-        errorElement.innerHTML = `
-          <p>${response.error}</p>
-          <p>Your cart has been saved.  Please contact our support team at <a href="help@macromeals.life">help@macromeals.life</a> to process your payment.</p>
-        `;
-
-        errorElement.style.display = 'block';
-      }
-
-    });
-
-  });
+  }
 
 }
