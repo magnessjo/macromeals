6fcc586cc6152eeebedd3bb2e510a285a9fb6a4d
Josh Magness <magnessjo@me.com>
2018-03-07 14:00:44 -0500

    Update the release process

--- deploy/release.js
+++ deploy/release.js
@@ -4,13 +4,10 @@ import shell from 'shelljs';
 import config from './config.json';
 import symlink from './symlink.js';
 
-shell.rm('-rf', 'build');
 shell.exec('webpack --env.NODE_ENV=production', { async: false }, function() {
   shell.rm('build/index.php');
   shell.rm('-rf', 'build/uploads');
 
   deploy.release(config);
   symlink();
-
-  shell.exec('webpack -w');
 });
