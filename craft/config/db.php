0b6a5111a2bf99491998fd660d6c71872a506c5e
Josh Magness <magnessjo@me.com>
2018-03-25 12:02:07 -0400

    Update database

--- craft/config/db.php
+++ craft/config/db.php
@@ -13,7 +13,7 @@ return array(
 	'server' => 'localhost',
 
 	// The name of the database to select.
-	'database' => 'macromeals',
+	'database' => 'macrocommerce',
 
 	// The database username to connect with.
 	'user' => 'root',
