c2f6f14faf25dd38b62f2c817ad591eec3c4682f
Josh Magness <magnessjo@me.com>
2018-04-17 20:29:31 -0400

    Update craft and commerce

--- craft/vendor/craftcms/plugins.php
+++ craft/vendor/craftcms/plugins.php
@@ -79,23 +79,6 @@ return array (
     'changelogUrl' => 'https://raw.githubusercontent.com/solspace/craft3-calendar/master/CHANGELOG.md',
     'hasCpSection' => true,
   ),
-  'craftcms/commerce' => 
-  array (
-    'class' => 'craft\\commerce\\Plugin',
-    'basePath' => $vendorDir . '/craftcms/commerce/src',
-    'handle' => 'commerce',
-    'aliases' => 
-    array (
-      '@craft/commerce' => $vendorDir . '/craftcms/commerce/src',
-    ),
-    'name' => 'Craft Commerce',
-    'version' => '2.0.0-beta.2',
-    'description' => 'An amazingly powerful and flexible e-commerce platform for Craft CMS.',
-    'developer' => 'Pixel & Tonic',
-    'developerUrl' => 'https://craftcommerce.com',
-    'developerEmail' => 'support@craftcms.com',
-    'documentationUrl' => 'https://github.com/craftcms/docs',
-  ),
   'craftcms/commerce-paypal' => 
   array (
     'class' => 'craft\\commerce\\paypal\\Plugin',
@@ -114,5 +97,22 @@ return array (
     'documentationUrl' => 'https://github.com/craftcms/commerce-paypal',
     'changelogUrl' => 'https://raw.githubusercontent.com/craftcms/commerce-paypal/master/CHANGELOG.md',
     'downloadUrl' => 'https://github.com/craftcms/commerce-paypal/archive/master.zip',
+  ),
+  'craftcms/commerce' => 
+  array (
+    'class' => 'craft\\commerce\\Plugin',
+    'basePath' => $vendorDir . '/craftcms/commerce/src',
+    'handle' => 'commerce',
+    'aliases' => 
+    array (
+      '@craft/commerce' => $vendorDir . '/craftcms/commerce/src',
+    ),
+    'name' => 'Craft Commerce',
+    'version' => '2.0.0-beta.3',
+    'description' => 'An amazingly powerful and flexible e-commerce platform for Craft CMS.',
+    'developer' => 'Pixel & Tonic',
+    'developerUrl' => 'https://craftcommerce.com',
+    'developerEmail' => 'support@craftcms.com',
+    'documentationUrl' => 'https://github.com/craftcms/docs',
   ),
 );
